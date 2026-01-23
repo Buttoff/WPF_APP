e0a62326a06fb13d3c59ab73fb9a233ab552a87e
Yarl <120281807+Buttoff@users.noreply.github.com>
2026-01-23 17:33:27 +0300

    lb1

---
+++ Web Api division/Web Api division/Controllers/CalculatorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Web_Api_division.Models;
+
+namespace WebApiDivision.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost("calculate")]
+        public IActionResult Calculate([FromBody] CalcRequest request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Request body is required");
+            }
+
+
+            double result = request.A / request.B;
+
+            return Ok(new { result = result, operation = "addition" });
+        }
+
+
+    }
+}
---
+++ Web Api division/Web Api division/Models/CalceRquest.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_Api_division.Models
+{
+    public class CalcRequest
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+    }
+}
